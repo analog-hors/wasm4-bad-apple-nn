3729af1e1cc9becd6e956e37836f45bc8338b08b
analog-hors <44050761+analog-hors@users.noreply.github.com>
2024-06-17 15:09:46 +1000

    Clean up write_frames.js script

--- write_frames.js
+++ write_frames.js
@@ -1,28 +1,45 @@
 const fs = require("fs");
+const child_process = require("child_process");
 
-let instance;
-const getByteSlice = (ptr, size) => {
-    const mem = instance.exports.memory.buffer;
-    return new Uint8Array(mem.slice(ptr, ptr + size));
-};
-const importObject = {
-    env: {
-        time: () => performance.now(),
-        print: (messagePtr, messageSize) => {
-            const bytes = getByteSlice(messagePtr, messageSize);
-            const message = Buffer.from(bytes, "utf-8").toString();
-            console.log(message);
-        },
-        write_file: (pathPtr, pathSize, bufferPtr, bufferSize) => {
-            const path = getByteSlice(pathPtr, pathSize);
-            const buffer = getByteSlice(bufferPtr, bufferSize);
-            fs.writeFileSync(path, buffer);
-        },
-    }
-};
+function buildModule() {
+    child_process.execFileSync(
+        "cargo",
+        [
+            "build",
+            "-p", "write-frames",
+            "--release",
+            "--target", "wasm32-unknown-unknown",
+        ],
+        { stdio: "inherit" },
+    );
+    const buffer = fs.readFileSync("target/wasm32-unknown-unknown/release/write_frames.wasm");
+    return new WebAssembly.Module(buffer);
+}
 
-const wasm = fs.readFileSync("target/wasm32-unknown-unknown/release/write_frames.wasm");
-(async () => {
-    instance = (await WebAssembly.instantiate(wasm, importObject)).instance;
+function runModule(module) {
+    let instance;
+    const getByteSlice = (ptr, size) => {
+        const mem = instance.exports.memory.buffer;
+        return new Uint8Array(mem.slice(ptr, ptr + size));
+    };
+    const importObject = {
+        env: {
+            time: () => performance.now(),
+            print: (messagePtr, messageSize) => {
+                const bytes = getByteSlice(messagePtr, messageSize);
+                const message = Buffer.from(bytes, "utf-8").toString();
+                console.log(message);
+            },
+            write_file: (pathPtr, pathSize, bufferPtr, bufferSize) => {
+                const path = getByteSlice(pathPtr, pathSize);
+                const buffer = getByteSlice(bufferPtr, bufferSize);
+                fs.writeFileSync(path, buffer);
+            },
+        }
+    };
+
+    instance = new WebAssembly.Instance(module, importObject);
     instance.exports.main();
-})();
+}
+
+runModule(buildModule());
